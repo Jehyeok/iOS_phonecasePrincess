655eb7c67fae9d64fa96aeb09b68021a02dadf44
Jehyeok <jehyeok@Jehyeoks-MacBook-Pro.local>
2013-05-24 16:10:37 +0900

    first commit

---
+++ iOS_phonecasePrincess/iOS_phonecasePrincess/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  iOS_phonecasePrincess
+//
+//  Created by Jehyeok on 5/24/13.
+//  Copyright (c) 2013 aurumplanet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
